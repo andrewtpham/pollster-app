6a549b08e2adcd1ff9a6b848aee132c7ed5e2918
andrewtpham <andrew.tc.pham@gmail.com>
2019-12-21 03:36:28 +0100

    adding questions and results

--- polls/admin.py
+++ polls/admin.py
@@ -3,18 +3,17 @@ from django.contrib import admin
 from .models import Question, Choice
 
 admin.site.site_header = "Pollster Admin"
-#admin.site.site_title = "Pollster Admin Area"
-#admin.site.index = "Welcome to the Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster Admin"
 
-class ChoiceInLine(admin.TabularInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [(None, {'fields': ['question_text']}),
-                ('Date Information', {'fields': ['publish_date'], 'classes': 
-                ['collapse']}),]
-    inLines = [ChoiceInLine]
+                 ('Date Information', {'fields': ['publish_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceInline]
 
 # admin.site.register(Question)
 # admin.site.register(Choice)
